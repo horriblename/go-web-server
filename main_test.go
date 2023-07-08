9610ff50a2eaed6d57bd95eda1e685b7118566ae
Ching Pei Yang <badnam3o.0@gmail.com>
2023-07-08 15:15:56 +0200

    refactor: generalize http requests

--- main_test.go
+++ main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -16,11 +15,19 @@ import (
 	"github.com/horriblename/go-web-server/db"
 )
 
+var gNoCheck *struct{} = nil
+
 // no I don't know how to mock servers properly shut up
 func TestServer(t *testing.T) {
 	url := "localhost:9000"
 
 	_ = os.Remove(DEBUG_DATABASE_FILE)
+
+	assertOk := func(err error) {
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+	}
 
 	serverErr := make(chan error, 1)
 	serverCfg := serverConfig{
@@ -35,128 +42,80 @@ func TestServer(t *testing.T) {
 
 	url = "http://" + url
 
-	testGetString(t, url+"/api/healthz", http.StatusOK, "OK")
-	testGet(t, url+"/app", http.StatusOK)
-	testGet(t, url+"/app/assets/logo.png", http.StatusOK)
+	assertOk(testHttpRequestString("GET", url+"/api/healthz", nil, http.StatusOK, "OK"))
+	assertOk(testHttpRequest("GET", url+"/app", nil, http.StatusOK, gNoCheck))
+	assertOk(testHttpRequest("GET", url+"/app/assets/logo.png", nil, http.StatusOK, gNoCheck))
 
 	// TODO check it returns 2
-	testGet(t, url+"/admin/metrics", http.StatusOK)
+	assertOk(testHttpRequest("GET", url+"/admin/metrics", nil, http.StatusOK, gNoCheck))
 
 	// test /api/chirp
 
 	var req PostChirpRequest
 	chirps_url := url + "/api/chirps"
-	testGet(t, chirps_url, http.StatusOK)
+	assertOk(testHttpRequest("GET", chirps_url, nil, http.StatusOK, gNoCheck))
 
 	req = PostChirpRequest{"Hello!"}
 	exp1 := "Hello!"
-	testPost(t, chirps_url, req, 201, &db.Chirp{Id: 1, Body: exp1})
+	assertOk(testHttpRequest("POST", chirps_url, req, 201, &db.Chirp{Id: 1, Body: exp1}))
 
 	req = PostChirpRequest{strings.Repeat(".", 141)}
-	testPost(t, chirps_url, req, 400, &genericFailMessage{"Chirp is too long"})
+	assertOk(testHttpRequest("POST", chirps_url, req, 400, &genericFailMessage{"Chirp is too long"}))
 
 	req = PostChirpRequest{"This is a keRfUfFle opinion I need to share with the world!"}
 	exp2 := "This is a **** opinion I need to share with the world!"
-	testPost(t, chirps_url, req, 201, &db.Chirp{Id: 2, Body: exp2})
+	assertOk(testHttpRequest("POST", chirps_url, req, 201, &db.Chirp{Id: 2, Body: exp2}))
 
 	expect := []db.Chirp{
 		{Id: 1, Body: exp1},
 		{Id: 2, Body: exp2},
 	}
-	testGetJson(t, chirps_url, http.StatusOK, expect)
+	assertOk(testHttpRequest("GET", chirps_url, nil, http.StatusOK, &expect))
 
 	// get chirp by id
-	testGetJson(t, url+"/api/chirps/1", http.StatusOK, expect[0])
+	assertOk(testHttpRequest("GET", url+"/api/chirps/1", nil, http.StatusOK, &expect[0]))
 	// TODO check returned result
-	testGet(t, url+"/api/chirps/100", http.StatusNotFound)
+	assertOk(testHttpRequest("GET", url+"/api/chirps/100", nil, http.StatusNotFound, gNoCheck))
 
 	users_url := url + "/api/users"
 	exp1 = "x@ymail.com"
 	pw1 := "04234"
 	req_user := PostUserRequest{exp1, pw1}
-	testPost(t, users_url, req_user, 201, &db.User{Id: 1, Email: exp1})
+	assertOk(testHttpRequest("POST", users_url, req_user, 201, &db.UserDTO{Id: 1, Email: exp1}))
 
 	exp2 = "abc@nomail.com"
 	pw2 := "10293"
 	req_user = PostUserRequest{exp2, pw2}
-	testPost(t, users_url, req_user, 201, &db.User{Id: 2, Email: exp2})
+	assertOk(testHttpRequest("POST", users_url, req_user, 201, &db.UserDTO{Id: 2, Email: exp2}))
 
 	login_url := url + "/api/login"
 	req_login := PostUserRequest{exp1, pw1}
-	testPost(t, login_url, req_login, 200, &db.User{Id: 1, Email: exp1})
+	assertOk(testHttpRequest("POST", login_url, req_login, 200, &db.UserDTO{Id: 1, Email: exp1}))
 
 	req_login = PostUserRequest{exp2, pw2}
-	testPost(t, login_url, req_login, 200, &db.User{Id: 2, Email: exp2})
+	assertOk(testHttpRequest("POST", login_url, req_login, 200, &db.UserDTO{Id: 2, Email: exp2}))
 
 	req_login = PostUserRequest{exp2, "wrong password"}
 	var nocheck *struct{} = nil
-	testPost(t, login_url, req_login, http.StatusUnauthorized, nocheck)
+	assertOk(testHttpRequest("POST", login_url, req_login, http.StatusUnauthorized, nocheck))
 }
 
-// The returned body must be `Close`d after use
-func httpGet(url string, code int) (body io.ReadCloser, err error) {
-	resp, err := http.Get(url)
+func testHttpRequestString(method string, url string, req any, code int, expect string) error {
+	resp, err := sendHttpRequest(method, url, req, code)
 	if err != nil {
-		resp.Body.Close()
-		return nil, err
+		return err
 	}
+	defer resp.Body.Close()
 
-	if resp.StatusCode != code {
-		resp.Body.Close()
-		return nil, errors.New(fmt.Sprintf(`got status %s, expected %d`, resp.Status, code))
-	}
-
-	return resp.Body, nil
-}
-
-func testGet(t *testing.T, url string, code int) {
-	body, err := httpGet(url, code)
+	got, err := io.ReadAll(resp.Body)
 	if err != nil {
-		t.Errorf("Getting: %s", err)
-		return
-	}
-	body.Close()
-}
-
-func testGetString(t *testing.T, url string, code int, expect string) {
-	body, err := httpGet(url, code)
-	if err != nil {
-		t.Errorf("Getting: %s", err)
-		return
-	}
-	defer body.Close()
-
-	got, err := io.ReadAll(body)
-	if err != nil {
-		t.Errorf("Reading response body: %s", err)
-		return
+		return fmt.Errorf("Reading response body: %s", err)
 	}
 	if string(got) != expect {
-		t.Errorf(`Expected %s, got %s`, expect, got)
-		return
-	}
-}
-
-func testGetJson[T any](t *testing.T, url string, code int, expect T) {
-	body, err := httpGet(url, code)
-	if err != nil {
-		t.Errorf("Getting: %s", err)
-		return
-	}
-	defer body.Close()
-
-	var got T
-	decoder := json.NewDecoder(body)
-	err = decoder.Decode(&got)
-
-	if err != nil {
-		t.Errorf(`Decoding json: %s`, err)
-		return
+		return fmt.Errorf(`Expected %s, got %s`, expect, got)
 	}
 
-	if !reflect.DeepEqual(got, expect) {
-		t.Errorf(`Expected %+v\nGot %+v`, expect, got)
-	}
+	return nil
 }
 
 type PostChirpRequest struct {
@@ -170,27 +129,40 @@ type PostUserRequest struct {
 	Password string `json:"password"`
 }
 
-func testPost[T, U any](t *testing.T, url string, req U, code int, expect *T) {
+func sendHttpRequest(method string, url string, req any, code int) (*http.Response, error) {
 	var dat []byte
 	var err error
 	dat, err = json.Marshal(req)
 	if err != nil {
 		panic("something went wrong")
 	}
-	resp, err := http.Post(url, "application/json", bytes.NewReader(dat))
+	httpReq, err := http.NewRequest(method, url, bytes.NewReader(dat))
+	if err != nil {
+		return nil, fmt.Errorf(`http.NewRequest: %s`, err)
+	}
+	httpReq.Header.Set("Content-Type", "application/json")
+	resp, err := (&http.Client{}).Do(httpReq)
 
 	if err != nil {
-		t.Errorf(`Posting /api/chirp: %s`, err)
-		return
+		return nil, fmt.Errorf(`Posting to %s: %w`, url, err)
 	}
 
 	if resp.StatusCode != code {
-		t.Errorf("Expected status code %d, got %d", code, resp.StatusCode)
-		return
+		return nil, fmt.Errorf("Expected status code %d, got %d", code, resp.StatusCode)
 	}
 
+	return resp, nil
+}
+
+func testHttpRequest[T any](method string, url string, req any, code int, expect *T) error {
+	resp, err := sendHttpRequest(method, url, req, code)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
 	if expect == nil {
-		return
+		return nil
 	}
 
 	var got T
@@ -199,11 +171,12 @@ func testPost[T, U any](t *testing.T, url string, req U, code int, expect *T) {
 	err = decoder.Decode(&got)
 
 	if err != nil {
-		t.Errorf(`Decoding response: %s`, err)
-		return
+		return fmt.Errorf(`Decoding response: %w`, err)
 	}
 
 	if !reflect.DeepEqual(got, *expect) {
-		t.Errorf(`Expected %+v\nGot %+v`, *expect, got)
+		return fmt.Errorf(`Expected %+v\nGot %+v`, *expect, got)
 	}
+
+	return nil
 }
